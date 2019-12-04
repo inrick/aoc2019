970fea2c11cf7f47470a7857bc68815305c2fb32
Fabian Carlström <inrick@gmail.com>
2019-12-04 11:24:48 +0100

    day04: faster
    Preallocate array for components. We know it is always 6 digits long, no
    need to zero between usages.

--- day04/main.go
+++ day04/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	const lo, hi = 359282, 820401
 
 	a, b := 0, 0
+	var nn [6]int
 Search:
 	for n := lo; n <= hi; n++ {
-		nn := split(n)
+		split(&nn, n)
 		va, vb := false, false
 		for i := 0; i < len(nn); {
 			j := i
@@ -43,12 +43,9 @@ Search:
 	fmt.Printf("b) %d\n", b)
 }
 
-func split(n int) []int {
-	cap := 1 + int(math.Floor(math.Log10(float64(n))))
-	elems := make([]int, cap)
-	for i := 0; i < cap; i++ {
-		elems[cap-1-i] = n % 10
+func split(nn *[6]int, n int) {
+	for i := 0; i < 6; i++ {
+		nn[5-i] = n % 10
 		n /= 10
 	}
-	return elems
 }
